b36c3fb7eec832f3bfa636ba407b86aee1b18924
Oded Goldberg <ogoldberg@ebay.com>
2020-10-12 10:30:12 +0300

    tar 3 ready

--- starting_from_scratch_lab/lab 37/lab_37_tar3.js
+++ starting_from_scratch_lab/lab 37/lab_37_tar3.js
@@ -1,29 +1,50 @@
-const game = [
-  [" ", " ", " "],
-  [" ", " ", " "],
-  [" ", " ", " "],
-];
-
-const game = [
-  ["O", "O", "O"],
-  ["X", "X", "X"],
+let game = [
   ["X", "O", "O"],
-];
+  ["O", "X", "O"],
+  ["O", "O", "X"],
+]; // diag
+
+let game = [
+    ["X", "O", "O"],
+    ["X", "O", "O"],
+    ["O", "O", "X"],
+  ]; // colm
+
+  let game = [
+    ["X", "X", "X"],
+    ["O", "X", "O"],
+    ["O", "O", "X"],
+  ]; // row
 
 const verdict = theWinnerTakesItAll(game);
 
 function theWinnerTakesItAll(game) {
+  let diagonal = [];
   for (let index = 0; index < game.length; index++) {
-    const row = game[index].reduce(function (a, b) {
+    const winnerRow = game[index].reduce(function (a, b) {
       return a === b ? a : !b;
     });
-    console.log(row);
+    if (winnerRow) {
+      console.log("winnerRow " + winnerRow);
+      return winnerRow;
+    }
+
+    const columnsAsLines = game.map((row) => row[index]);
+    const winnerCol = columnsAsLines.reduce(function (a, b) {
+      return a === b ? a : !b;
+    });
+    if (winnerCol) {
+      console.log("winnerCol " + winnerCol);
+      return winnerCol;
+    }
+    if (!diagonal.includes("O") && !diagonal.includes("X")) {
+        console.log(game[index][index]);
+      diagonal.push(game[index][index]);
+    }
   }
 
-
-  console.log(game.length);
-  console.log(game[0].length);
-  // need to handle row
-  // need to handle column
-  // need to handle diag
+  if (diagonal.length === 1) {
+    console.log("winnerDiag " + diagonal[0]);
+    return diagonal[0];
+  }
 }
