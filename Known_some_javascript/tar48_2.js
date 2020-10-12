c9cad31bde058a5c68bd52cd8277ca0e786b0b7e
Oded Goldberg <ogoldberg@ebay.com>
2020-10-12 16:03:01 +0300

    commit my code

---
+++ Known_some_javascript/tar48_2.js
@@ -0,0 +1,14 @@
+var b1 = document.querySelector('#btn1');
+var b2 = document.querySelector('#btn2');
+var panel = document.querySelector('p');
+
+function writeText(text) {
+  /* Write Code Here To Make The Program Work */
+  return function curried_func(e) {
+    // do something here
+    panel.innerHTML = text;
+}
+}
+
+b1.addEventListener('click', writeText('Yo'));
+b2.addEventListener('click', writeText('Nice To Meet You'));
