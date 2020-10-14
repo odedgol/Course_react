97151d70c1a81323e809b34fe83b37b0725f8a15
Oded Goldberg <ogoldberg@ebay.com>
2020-10-14 08:51:02 +0300

    commit files

---
+++ starting_from_scratch_lab/lab_4/lab4_1.js
@@ -0,0 +1,11 @@
+var container = document.querySelector('div');
+
+for (let i=0; i < 4; i++) {
+  var btn = document.createElement('button');
+  btn.addEventListener('click', function() {
+    alert(i);
+  });
+  btn.textContent = i;
+  container.appendChild(btn);
+}
+
