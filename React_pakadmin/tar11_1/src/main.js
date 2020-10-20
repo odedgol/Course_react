6a1a13260bd4dfd89cf2d93f272685aa9a7ba9b1
Oded Goldberg <ogoldberg@ebay.com>
2020-10-20 09:45:01 +0300

    Fixed PR

--- React_pakadmin/tar11_1/src/main.js
+++ React_pakadmin/tar11_1/src/main.js
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import MultiInput from "./multiInput";
 import "../css/main.css";
 
 const App = () => {
-  const [inputText, setInputText] = useState("test");
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <MultiInput inputText={inputText} setInputText={setInputText} />
-      <MultiInput inputText={inputText} setInputText={setInputText} />
-      <MultiInput inputText={inputText} setInputText={setInputText} />
-      <MultiInput inputText={inputText} setInputText={setInputText} />
-      <MultiInput inputText={inputText} setInputText={setInputText} />
+    <div>
+      <MultiInput />
     </div>
   );
 };
--- React_pakadmin/tar11_1/src/multiInput.js
+++ React_pakadmin/tar11_1/src/multiInput.js
@@ -1,19 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function MultiInput({ inputText, setInputText }) {
-  
+export default function MultiInput() {
+  const [inputText, setInputText] = useState();
+
   function onChangeText(e) {
     setInputText(e.target.value);
   }
 
   return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: "50%",
+        justifyContent: 'start'
+
+      }}
+    >
       <div>
-    <input
-      type="text"
-      value={inputText}
-      onChange={onChangeText}
-      style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-    />
+        <input
+          type="text"
+          value={inputText}
+          onChange={onChangeText}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={onChangeText}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={onChangeText}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={onChangeText}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={onChangeText}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        />
+      </div>
     </div>
   );
 }
